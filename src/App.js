34925b374bb32d1e1e2d1624589422d701d29a52
leehyoungjoon <92970761+leehyoungjoon@users.noreply.github.com>
2022-04-11 23:58:27 +0900

    최초커밋

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [kossie, setKossie] = useState(0);
+  useEffect(() => {
+    console.log(count);
+  }, [count, kossie]);
+
+  useEffect(() => {
+    console.log("first rendering");
+  }, []);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="App">
+      <h1>can do react</h1>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => setKossie(kossie + 1)}>Click1</button>
+    </div>
+  );
+}
+
+export default App;
